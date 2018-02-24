047b06dc7c5d360d93494605f78ed344f8431642
Iampietro <iampietromatias@gmail.com>
2018-02-24 15:31:37 -0300

    Working with classes done

--- 02-oop-inheritance/app.js
+++ 02-oop-inheritance/app.js
@@ -20,33 +20,48 @@ class EventEmitter {
         }
     }
 
-    off(event, givenCallback) {
-        // If the given event exists
+    off(event, givenCallback) { /* since the method recives the callback,
+                                             we delete just that, instead of the whole event */
         if (this.events[event]) {
-            delete this.events[event];
+            let index = this.events[event].indexOf(givenCallback);
+            if (index !== -1) {
+                this.events[event].splice(index,1);
+            }
         }
     }
 }
 
 
-class Movie {
+class Movie extends EventEmitter{
 
     constructor(title, year, duration) {
+        super();
         this.title = title;
         this.year = year;
         this.duration = duration;
+        this.actors = [];
     }
 
     play() {
-        console.log("You are reproducing: " + this.title);
+        this.emit("play");
     }
 
     pause() {
-        console.log("You stopped the movie");
+        this.emit("pause");
     }
 
     resume() {
-        console.log("pop corn is ready");
+        this.emit("resume");
+    }
+
+    addCast(cast){
+        if (cast instanceof Array) {
+            for (let i = 0; i < cast.length ; i++) {
+                this.actors.push(cast[i]);
+            }
+        } else {
+            this.actors.push(cast);
+        }
     }
 }
 
@@ -57,3 +72,28 @@ class Actor {
         this.age = age;
     }
 }
+
+class Logger { 
+
+    constructor() {}
+
+    log(info) {
+        console.log("The event: '" +info+ "' has been triggered!");
+    }
+}
+
+const terminator = new Movie('Terminator I', 1985, 60);
+const arnold = new Actor('Arnold Schwarzenegger', 50);
+const actors = [
+    new Actor('Paul Winfield', 50),
+    new Actor('Michael Biehn', 50),
+    new Actor('Linda Hamilton', 50)
+];
+const logger = new Logger();
+
+terminator.addCast(arnold);
+terminator.addCast(actors);
+
+terminator.on("play", logger.log);
+
+terminator.play();
